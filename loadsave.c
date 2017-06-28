60cfda4fb9c921d3412c54c2496a8d5012ff3e13
Christian C. Romero <id.32@outlook.com>
2017-06-28 23:14:14 +0200

    Load and Save functions finished.

--- loadsave.c
+++ loadsave.c
@@ -13,39 +13,38 @@
 
 bool load_game(Game_t *game) {
   FILE *file = fopen(GAME_FILE, "rb");
-  if (file != NULL) {
-    fread(game, sizeof(Game_t), 1, file);
-    fclose(file);
-    return true;
-  } else return false;
+  if (file != NULL) return false;
+  fread(game, sizeof(Game_t), 1, file);
+  fclose(file);
+  return true;
 }
 
 bool save_game(Game_t game) {
   FILE *file = fopen(GAME_FILE, "wb");
-  if (file != NULL) {
-    fwrite(&game, sizeof(Game_t), 1, file);
-    fclose(file);
-    return true;
-  } else return false;
+  if (file != NULL) return false;
+  fwrite(&game, sizeof(Game_t), 1, file);
+  fclose(file);
+  return true;
 }
 
 bool load_scores(Scores_t *scores) {
   FILE *file = fopen(SCORE_FILE, "r");
-  if (file == NULL) {
-    return false;
+  if (file == NULL) return false;
+  while (!feof(file) && scores->num < SCORE_MAX) {
+    if (fscanf(file, "%s\t%d\n", scores->score[scores->num].name, &scores->score[scores->num].points) == 2) {
+      scores->num++;
+    }
   }
+  fclose(file);
   return true;
 }
 
 bool save_scores(Scores_t scores) {
   FILE *file = fopen(SCORE_FILE, "w");
-  if (file == NULL) {
-    return false;
+  if (file == NULL) return false;
+  for (int i = 0; i < scores.num; i++) {
+    fprintf(file, "%s\t%d\n", scores.score[i].name, scores.score[i].points);
   }
+  fclose(file);
   return true;
-}
-
-void sort_scores(Scores_t *scores) {
-  printf("sort_scores()");
-  getchar();
 }
