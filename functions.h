e15189fce7c38435d6e6315df581d7d2af2a2efd
Christian C. Romero <id.32@outlook.com>
2017-06-28 23:33:37 +0200

    Updated header file.

--- functions.h
+++ functions.h
@@ -15,16 +15,14 @@
 
 // viusal.c
 int main_menu(bool game);
-void show_menu(bool game);
-bool valid_option(bool game, int option);
-int get_mode();
-int get_dim();
-int get_ini();
+int mode_menu();
+int size_menu();
+int init_menu();
 void print_table(Table_t table);
 void print_tables(Table_t table1, Table_t table2);
-void scoreboard(Scores_t records);
+void print_scoreboard(Scores_t scores);
+const char *shot_to_string(Shot_e shot);
 void pause();
-void print_shoot(Shot_e shoot);
 
 // initialization.c
 void new_game(Game_t *game);
@@ -39,15 +37,22 @@ void scpy(char *c, char *s);
 
 // logic.c
 bool play_game(Game_t *game, Scores_t *scores);
-Shot_e shoot(Table_t table, Coord_t coord);
+bool play_game_solo_AI(Game_t *game, Scores_t *scores);
+bool play_game_solo_Player(Game_t *game, Scores_t *scores);
+bool play_game_Player_vs_AI(Game_t *game, Scores_t *scores);
+Shot_e shoot(Table_t *ships_table, Coord_t coord);
+bool is_ship_sunk(Table_t ships_table, Coord_t coord);
+bool is_water(Cell_e cell);
+void update_shots_table(Table_t *shots_table, Coord_t coord, Shot_e result);
+void unveil_surroundings(Table_t *shots_table, Coord_t coord);
 Coord_t get_coord_from_Player(Table_t table);
 Coord_t get_coord_from_AI(Table_t table);
+int get_score(Player_t player);
 
 // loadsave.c
 bool load_game(Game_t *game);
 bool save_game(Game_t game);
 bool load_scores(Scores_t *scores);
 bool save_scores(Scores_t scores);
-void sort_scores(Scores_t *scores);
 
 #endif //BATTLESHIP_FUNCIONS_H
